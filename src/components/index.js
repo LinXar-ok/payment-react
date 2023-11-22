309037355358813e5f218ed85efd90e506eda78e
Linus Kexordzu <134380920+LinXar-ok@users.noreply.github.com>
2023-11-22 08:57:38 +0000

    update project
    Adding the files and codes to the repository

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Footer,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Hero,
+    Testimonials,
+};
